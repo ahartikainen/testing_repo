3e1adb4b1b284cefd0d625b299edb2c2965695ff
Ari Hartikainen <ahartikainen@users.noreply.github.com>
2022-01-17 10:53:22 +0200

    Update __init__.py

--- arviz/__init__.py
+++ arviz/__init__.py
@@ -1,6 +1,6 @@
 # pylint: disable=wildcard-import,invalid-name,wrong-import-position
 """ArviZ is a library for exploratory analysis of Bayesian models."""
-__version__ = "custom-dev"
+__version__ = "1.11.1-dev"
 
 import logging
 import os
